d8477fc1908b71a9fe63f5150d1385ff5ea94b28
Samarth Paliwal <ram.paliwal49@gmail.com>
2020-12-10 01:02:27 +0530

    backend (django) quiz, remove all previous stuff

--- quizDjango/quizy/serializers.py
+++ quizDjango/quizy/serializers.py
@@ -1,9 +1,9 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 
-# from quizy.models import Quiz
+from quizy.models import Quiz
 
 
-# class QuizSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = Quiz
-# 		fields = ['start_date', 'end_date', 'title', 'duration']
+class QuizSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Quiz
+		fields = ['start_date', 'end_date', 'title', 'duration',]
--- quizDjango/quizy/urls.py
+++ quizDjango/quizy/urls.py
@@ -2,28 +2,12 @@ from django.urls import path
 from .views import *
 
 #new
-# from quizy.api.views import(
-# 	# api_detail_blog_view,
-# 	# api_update_blog_view,
-# 	# api_delete_blog_view,
-# 	api_create_quiz_view,
-# )
+from quizy.api.views import(
+	api_create_quiz_view,
+)
 
-app_name = 'quiz_app'
+app_name = 'quizy'
 
 urlpatterns = [
-    #new
-    path('quiz/create-quiz', create_quiz, name="create_quiz"),
-    
-    #api
-    # path('quiz/create', api_create_quiz_view, name="create"),
-
-    # past
-    path('quiz/available', AvailableQuiz.as_view(), name="available_quiz"),
-    path('quiz/check/<quiz_id>', attempt_quiz, name="attempt_quiz"),
-    path('quiz/result/<quiz_id>', QuizResult.as_view(), name="quiz_result"),
-    # path('ajax/live/quiz',save_answer, name="ajax_save_live_quiz"),
-    path('live/quiz/<quiz_id>',liveQuiz, name="live_quiz_new"),
-    path('live/quiz/end/<quiz_id>',end_quiz, name="end_quiz"),
-    path('quiz/results',Results.as_view(), name="results"),
+    path('quiz/create', api_create_quiz_view, name="create"),
 ]
